f97516c6a1e6c4a3a5455e686ffd0e7751790d5c
Emma <enabojohn@gmail.com>
2020-03-20 11:08:39 +0300

    Added client input validation to the course.push() method

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 // });
 
 
+//express.json return a middleware, app.use tells the app to use
+// that middleware
+app.use(express.json());
+
 
 const courses = [
 { id:1, name: 'course1'},
@@ -15,11 +19,21 @@ const courses = [
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
-
+// Http get request
 app.get('/api/courses/:id', (req, res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
    if( !course ) res.status(404).send('Error:Course with the given id not found');
    res.send(course);
+});
+
+//Http post request
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
